139f493e5b624f1f15de994823f6675e5225ddbc
Andrey Zaika <anzaika@gmail.com>
2011-10-12 09:54:56 +0400

    add new examples

---
+++ spec/mysql_info/mysql_info_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+module MysqlInfo
+  describe MysqlInfo do
+    describe "#all_privileges?" do
+      it "should return true if passed username has all privileges"
+    end
+    describe "#users" do
+      it "should return usernames with any privileges on server"
+    end
+  end
+end
